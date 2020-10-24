61796d6ee621e2419b4dd1604e6fd880ffc592fd
Danilkashtan <36625284+Danilkashtan@users.noreply.github.com>
2020-10-24 13:44:52 +0300

    [*] add danilkashtan mapping

---
+++ program.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/v2')
+def another_text():
+    return 'Another Text'
+
+@app.route('/danilkashtan')
+def danilkashtan():
+    return 'Асоян Данила'
