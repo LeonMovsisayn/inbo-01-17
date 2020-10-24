eb2a38d840105beead5996fbb8862e2d79486c7f
Andrey Alekseev <andrey.alekseev00@yandex.ru>
2020-10-24 14:10:29 +0300

    Add program

--- program.py
+++ program.py
@@ -5,7 +5,28 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-
+<<<<<<< HEAD
 @app.route('/v2')
 def hello_world_v2():
-    return 'Another text'
+    return 'Hello, World! v2'
+
+@app.route('/alekseev')
+def alekseev():
+    return 'Alekseev Andrey INBO-01-17'
+=======
+@app.route('/trm')
+def hello_world_trm():
+    return 'Hello, my name is Alexandr'
+
+@app.route('/v2')
+def another_text():
+    return 'Another Text'
+
+@app.route('/capchik')
+def capchik():
+    return 'Макущенко Максим'
+
+@app.route('/danilkashtan')
+def danilkashtan():
+    return 'Асоян Данила'
+>>>>>>> master
