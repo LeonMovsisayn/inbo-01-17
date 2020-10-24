47930cb0f3771f8e80abbea01d609cac88bb7431
Valery Kretov <valera.kretov@gmail.com>
2020-10-24 14:10:14 +0300

    Fixed

--- program.py
+++ program.py
@@ -5,7 +5,21 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/trm')
+def hello_world_trm():
+    return 'Hello, my name is Alexandr'
 
 @app.route('/v2')
-def hello_world_v2():
-    return 'Another text'
+def another_text():
+    return 'Another Text'
+
+@app.route('/KretOFF')
+def KretOFF():
+    return 'Кретов Валерий Витальевич'
+@app.route('/capchik')
+def capchik():
+    return 'Макущенко Максим'
+
+@app.route('/danilkashtan')
+def danilkashtan():
+    return 'Асоян Данила'
